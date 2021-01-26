cb22dab757c81b3b7aa550524235d65b07c2c8d2
Marwan <marwan.esmaail85@gmail.com>
2021-01-26 09:52:17 +0100

    Creating currencies dropdown list

--- src/CurrenciesListscreen/CurrenciesList.js
+++ src/CurrenciesListscreen/CurrenciesList.js
@@ -1,21 +1,33 @@
 import React, { useContext } from "react";
 import { CurrenciesContext } from "../Components/CurrenciesContext";
-
+import "bootstrap/dist/css/bootstrap.min.css";
 
 const CurrenciesList = () => {
   const currencies = useContext(CurrenciesContext);
-  console.log(currencies);
   return (
     <div>
-      <ul>
+      <h2 style={h2Style}>Please choose a currency</h2>
+      <select className="browser-default custom-select" style={selectStyle}>
         {currencies &&
           currencies.map((currency) => {
-            return <li key={currency.id}>{currency.id}</li>;
+            return <option key={currency.id}>{currency.id}</option>;
           })}
-      </ul>
+      </select>
     </div>
   );
 };
 
+const selectStyle = {
+  marginTop: "7%",
+  width: "50%",
+  border: 'bold',
+};
+
+const h2Style = {
+  fontFamily: "Apple Color Emoji ",
+  fontSize: 20,
+  fontWeight: "bold",
+  marginTop :'10%',
+};
 
 export default CurrenciesList;
