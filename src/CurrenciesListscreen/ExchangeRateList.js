69faea8647041dfa9db6c2569569697e29a127af
Marwan <marwan.esmaail85@gmail.com>
2021-02-01 10:31:31 +0100

    Adding Seacrh Bar

--- src/CurrenciesListscreen/ExchangeRateList.js
+++ src/CurrenciesListscreen/ExchangeRateList.js
@@ -2,6 +2,8 @@ import React, { useContext, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import { CurrenciesContext } from "../Components/CurrenciesContext";
 import Bootsrap from "react-bootstrap";
+import Form from "react-bootstrap/Form";
+import Col from "react-bootstrap/Col";
 
 function ExchangeRateList() {
   const { fetchExchangeRate, exchangeRates } = useContext(CurrenciesContext);
@@ -13,30 +15,65 @@ function ExchangeRateList() {
 
   return (
     <div style={divStyle}>
-      {exchangeRates &&
-        Object.keys(exchangeRates.rates).map((key, i) => (
-          <p key={i}>
-            <span className ="badge badge-pill badge-primary" style={spanStyle}>
-              {key} &nbsp;
-            </span>
-            <span className ="badge badge-pill badge-warning" style={spanStyle}>
-              {exchangeRates.rates[key]}
-            </span>
-          </p>
-        ))}
+      <Form>
+        <Form.Row>
+          <Form.Group as={Col} style={groupStyle}>
+            <Form.Label style={labelStyle}>Seacrh Curreny</Form.Label>
+            <Form.Control
+              type="text"
+              placeholder="Type here"
+            />
+          </Form.Group>
+        </Form.Row>
+      </Form>
+
+      <div>
+        {exchangeRates &&
+          Object.keys(exchangeRates.rates).map((key, i) => (
+            <p key={i}>
+              <span
+                className="badge badge-pill badge-primary"
+                style={spanStyle}
+              >
+                {key} &nbsp;
+              </span>
+              <span
+                className="badge badge-pill badge-warning"
+                style={spanStyle}
+              >
+                {exchangeRates.rates[key]}
+              </span>
+            </p>
+          ))}
+      </div>
     </div>
   );
 }
 
 const spanStyle = {
   fontSize: 13,
-  fontFamily: "DejaVu Sans Mono, monospace",
+  fontFamily: "DejaVu Sans Mono, monospace", 
   wordSpacing: "5 em",
 };
 
+const labelStyle = {
+  fontSize: 14,
+  fontFamily: "DejaVu Sans Mono, monospace",
+  wordSpacing: "5 em",
+  fontWeight: "bold",
+};
 const divStyle = {
-  marginTop: "30%",
+  marginTop: "10%",
   marginBottom: "10%",
-  
+
+};
+
+const groupStyle = {
+  display: "flex",
+  justifyContent: "center",
+  width: "70%",
+  marginLeft: "15%",
+  marginRight: "15%",
+  fontSize: 12,
 };
 export default ExchangeRateList;
