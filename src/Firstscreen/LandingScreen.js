cddd8fbf6f91809ea665eb9351df8db32112c5f6
Marwan <marwan.esmaail85@gmail.com>
2021-02-04 13:06:22 +0100

    Converting feature

--- src/Firstscreen/LandingScreen.js
+++ src/Firstscreen/LandingScreen.js
@@ -1,6 +1,6 @@
 import React from "react";
 import DateAndTime from "./DateAndTime";
-import DollarAndEuroPrice from "./Dollar and Euro price";
+
 import Icon from "./Icon";
 import LoginButton from "./LoginButton";
 import RegistrationLink from './RegistrationLink';
@@ -10,7 +10,6 @@ function LandingScreen() {
     <div>
       <DateAndTime />
       <Icon />
-      <DollarAndEuroPrice />
       <LoginButton />
       <RegistrationLink/>
     </div>
