e053592ff1541d9047b9c745563481f439a97d53
marwanEsm <marwan.esmaail85@gmail.com>
2024-04-25 18:08:46 +0200

    replace context provider with dynamic fetch

--- src/components/listOfCurrencies/ListOfCurrencies.jsx
+++ src/components/listOfCurrencies/ListOfCurrencies.jsx
@@ -1,33 +1,38 @@
-import React, { useContext } from "react";
+import React, { useEffect, useContext, useState } from "react";
 import { CurrenciesContext } from "../../context/CurrenciesContext";
-import styles from "./ListOfCurrencies.module.scss"
+import styles from "./ListOfCurrencies.module.scss";
+import AsyncSelect from "react-select";
+import axios from "axios";
 
-
-//TODO: change all function to arrow functions
 const ListOfCurrencies = ({ onCurrencySelect }) => {
+    const [options, setOptions] = useState(null);
 
-    const { currencies } = useContext(CurrenciesContext);
+    const loadCurrencies = () => {
+        axios.get("https://api.coinbase.com/v2/currencies")
+            .then(response => {
+                if (response.data) {
+                    setOptions(response.data.data.map(currency => ({ value: currency.id, label: currency.name })));
+                }
+            })
+            .catch(error => {
+                console.error("Error fetching currencies:", error);
+            });
+    };
+
+    useEffect(() => {
+        loadCurrencies();
+    }, []);
 
     return (
         <div className={styles.container}>
-            <select
-                onChange={(e) => onCurrencySelect(e.target.value)}
-                className="browser-default custom-select"
-            >
-                {currencies &&
-                    currencies.map((currency) =>
-                        <option key={currency.id} value={currency.id}>
-                            {currency.id}
-                        </option>
-                    )}
-            </select>
+            <AsyncSelect
+                options={options}
+                onChange={onCurrencySelect}
+                className={styles.select}
+                placeholder="Select a currency"
+            />
         </div>
     );
-}
-
-
-
-
-
+};
 
 export default ListOfCurrencies;
