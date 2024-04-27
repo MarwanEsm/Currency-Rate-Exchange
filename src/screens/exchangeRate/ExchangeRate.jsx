ba2fca590e06df5662541908e1876c59c835d735
marwanEsm <marwan.esmaail85@gmail.com>
2024-04-27 18:06:53 +0200

    add function tp insert comma

--- src/screens/exchangeRate/ExchangeRate.jsx
+++ src/screens/exchangeRate/ExchangeRate.jsx
@@ -1,12 +1,18 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 import styles from "./ExchangeRateList.module.scss"
-
 const ExchangeRateList = ({ toCurrency, fromCurrency }) => {
-
     const [exchangeRates, setExchangeRates] = useState(null);
-    const [amount, setAmount] = useState(null)
-    console.log(amount);
+    const [amount, setAmount] = useState(null);
+
+    const numberWithCommas = (x) => {
+        if (!x) return "";
+        x = x.toString();
+        var pattern = /(-?\d+)(\d{3})/;
+        while (pattern.test(x))
+            x = x.replace(pattern, "$1,$2");
+        return x;
+    };
 
     const loadExchangeRate = () => {
         axios.get(`https://api.coinbase.com/v2/exchange-rates?currency=${toCurrency?.value}`)
@@ -18,34 +24,33 @@ const ExchangeRateList = ({ toCurrency, fromCurrency }) => {
             .catch(error => {
                 console.error("Error fetching exchange rates:", error);
             });
-    }
+    };
 
     useEffect(() => {
         loadExchangeRate();
     }, []);
 
-
-    return <>
-        {exchangeRates && toCurrency && exchangeRates[toCurrency.value] &&
-            <div className={styles.container}>
-                <div className={styles.inputWrapper}>
-                    <strong>{`${fromCurrency.value}`}</strong>
-                    <input
-                        type="number"
-                        placeholder={`Amount ${fromCurrency.value}`}
-                        value={amount}
-                        onChange={(e) => setAmount(e.target.value)}
-                    />
+    return (
+        <>
+            {exchangeRates && toCurrency && exchangeRates[toCurrency.value] && (
+                <div className={styles.container}>
+                    <div className={styles.inputWrapper}>
+                        <strong>{fromCurrency.value}</strong>
+                        <input
+                            type="text"
+                            placeholder="Amount"
+                            value={numberWithCommas(amount)}
+                            onChange={(e) => setAmount(e.target.value.replace(/\D/g, ''))}
+                        />
+                    </div>
+                    <span>
+                        <label>Exchange Rate</label>
+                        <b>{parseFloat(exchangeRates[toCurrency.value]).toFixed(4)}</b>
+                    </span>
                 </div>
-                <span>
-                    <label>Exchange Rate</label>{parseFloat(exchangeRates[toCurrency.value]).toFixed(4)}
-                </span>
-            </div>
-        }
-    </>
+            )}
+        </>
+    );
+};
 
-
-}
-
-
-export default ExchangeRateList
+export default ExchangeRateList;
